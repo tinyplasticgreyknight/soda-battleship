6204b3bb05735cd4b943966169c52aa9f60fd2e5
GreyKnight <tinyplasticgreyknight@yahoo.com>
2013-06-10 15:25:58 +0100

    constants for board size

--- Battleship_Test_Run_100613.py
+++ Battleship_Test_Run_100613.py
@@ -2,8 +2,10 @@ from random import randint
 
 board = []
 
-for x in range(0, 5):
-    board.append(["O"] * 5)
+BOARDW, BOARDH = (5, 5)
+
+for x in range(0, BOARDH):
+    board.append(["O"] * BOARDW)
 
 def print_board(board):
     for row in board:
@@ -12,10 +14,10 @@ def print_board(board):
 print_board(board)
 
 def random_row(board):
-    return randint(0, len(board) - 1)
+    return randint(0, BOARDH - 1)
 
 def random_col(board):
-    return randint(0, len(board[0]) - 1)
+    return randint(0, BOARDW - 1)
 
 ship_row = random_row(board)
 ship_col = random_col(board)
@@ -31,7 +33,7 @@ if guess_row == ship_row and guess_col == ship_col:
 elif board[int(guess_row)][int(guess_col)] == "X":
     print "You guessed that one already."
 else:
-    if guess_row < 0 or guess_row > len(board)-1 or guess_col < 0 or guess_col > len(board[0])-1:
+    if guess_row < 0 or guess_row > BOARDH-1 or guess_col < 0 or guess_col > BOARDW-1:
         print "Oops, that's not even in the ocean."
     else:
         print "You missed my battleship!"
