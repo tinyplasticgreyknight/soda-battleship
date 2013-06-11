3df30cef45198c038bbb478f7f0cd2d6500827ea
GreyKnight <tinyplasticgreyknight@yahoo.com>
2013-06-11 23:10:45 +0100

    implement Board.to_string method

--- board.py
+++ board.py
@@ -1,17 +1,20 @@
 from random import randint
 
 class Board:
-    def __init__(self, width, height):
-        self.grid = []
-        for x in range(0, height):
-            self.grid.append(["O"] * width)
-        self.width  = width
-        self.height = height
-    def is_a_hit(self, row, col):
-        pass
-    def is_in_ocean(self, row, col):
-        pass
-    def was_already_guessed(self, row, col):
-        pass
-    def to_string(self):
-        pass
+	def __init__(self, width, height):
+		self.grid = []
+		for x in range(0, height):
+			self.grid.append(["O"] * width)
+		self.width  = width
+		self.height = height
+	def is_a_hit(self, row, col):
+		pass
+	def is_in_ocean(self, row, col):
+		pass
+	def was_already_guessed(self, row, col):
+		pass
+	def to_string(self):
+		rowstrs = []
+		for row in self.grid:
+			rowstrs.append(" ".join(row))
+		return "\n".join(rowstrs)
