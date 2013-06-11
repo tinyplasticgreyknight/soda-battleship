ae8488dbcc9ce2ef53fe88d36dfd438cc47bc4d3
kcooke1983 <kcooke1983@gmail.com>
2013-06-11 21:19:07 +0100

    tab > space

--- Battleship_Test_Run_100613.py
+++ Battleship_Test_Run_100613.py
@@ -22,20 +22,20 @@ def random_col(board):
 ship_row = random_row(board)
 ship_col = random_col(board)
 
-for turn in range(4): #loop for 4 guessest
+for turn in range(4): #loop for 4 guesses
 	guess_row = int(raw_input("Guess Row:"))
 	guess_col = int(raw_input("Guess Col:"))
 
 
 	if (0 <= guess_row < BOARDH) and (0 <= guess_col < BOARDW):
-    	if guess_row == ship_row and guess_col == ship_col:
-    	    print "Congratulations! You sank my battleship!"
-    	elif board[guess_row][guess_col] == "X":
-    	    print "You guessed that one already."
-    	else:
-    	    print "You missed my battleship!"
-    	    board[guess_row][guess_col] = "X"
-    	    print_board(board)
+		if guess_row == ship_row and guess_col == ship_col:
+			print "Congratulations! You sank my battleship!"
+		elif board[guess_row][guess_col] == "X":
+			print "You guessed that one already."
+		else:
+			print "You missed my battleship!"
+			board[guess_row][guess_col] = "X"
+			print_board(board)
 	else:
-    	print "Oops, that's not even in the ocean."
+		print "Oops, that's not even in the ocean."
 
