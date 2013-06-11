2c5d83d4366fb72f8e7f949795122ffff93f45f4
GreyKnight <tinyplasticgreyknight@yahoo.com>
2013-06-11 21:43:21 +0100

    reorder if-else tree, factor out max turns magic number.  Normalise rest of indentation to tabs.

--- Battleship_Test_Run_100613.py
+++ Battleship_Test_Run_100613.py
@@ -5,38 +5,42 @@ board = []
 BOARDW, BOARDH = (5, 5)
 
 for x in range(0, BOARDH):
-    board.append(["O"] * BOARDW)
+	board.append(["O"] * BOARDW)
 
 def print_board(board):
-    for row in board:
-        print " ".join(row)
+	for row in board:
+		print " ".join(row)
 
 print_board(board)
 
 def random_row(board):
-    return randint(0, BOARDH - 1)
+	return randint(0, BOARDH - 1)
 
 def random_col(board):
-    return randint(0, BOARDW - 1)
+	return randint(0, BOARDW - 1)
 
 ship_row = random_row(board)
 ship_col = random_col(board)
 
-for turn in range(4): #loop for 4 guesses
-	print turn+1
+MAX_TURNS = 4
+
+for turn in range(1,MAX_TURNS+1): #loop for 4 guesses
+	print turn
 	guess_row = int(raw_input("Guess Row:"))
 	guess_col = int(raw_input("Guess Col:"))
 
 
-	if (0 <= guess_row < BOARDH) and (0 <= guess_col < BOARDW):
-		if guess_row == ship_row and guess_col == ship_col:
-			print "Congratulations! You sank my battleship!"
+	if guess_row == ship_row and guess_col == ship_col:
+		print "Congratulations! You sank my battleship!"
+	else:
+		if turn==MAX_TURNS:
+			print "*** Game Over ***"
+		elif not ((0 <= guess_row < BOARDH) and (0 <= guess_col < BOARDW)):
+			print "Oops, that's not even in the ocean."
 		elif board[guess_row][guess_col] == "X":
 			print "You guessed that one already."
 		else:
 			print "You missed my battleship!"
 			board[guess_row][guess_col] = "X"
 			print_board(board)
-	else:
-		print "Oops, that's not even in the ocean."
 
