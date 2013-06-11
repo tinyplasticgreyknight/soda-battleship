f0e08ef03430db55dbd086e4920531b2700f201a
kcooke1983 <kcooke1983@gmail.com>
2013-06-11 23:09:52 +0100

    defining functions

--- board.py
+++ board.py
@@ -1,17 +1,31 @@
 from random import randint
 
 class Board:
-    def __init__(self, width, height):
-        self.grid = []
-        for x in range(0, height):
-            self.grid.append(["O"] * width)
-        self.width  = width
-        self.height = height
-    def is_a_hit(self, row, col):
-        pass
-    def is_in_ocean(self, row, col):
-        pass
-    def was_already_guessed(self, row, col):
-        pass
-    def to_string(self):
-        pass
+	def __init__(self, width, height):
+		self.grid = []
+		for x in range(0, height):
+			self.grid.append(["O"] * width)
+		self.width  = width
+		self.height = height
+		self.ship_row = randint(0, height -1)
+		self.ship_col = randint(0, width -1)
+	def is_a_hit(self, row, col):
+		if row == self.ship_row and row == self.ship_col:
+			return True
+		else:
+			self.grid[row][col] = "X"
+			return False
+	def is_in_ocean(self, row, col):
+		if type(row) != int or type(col) != int:
+			return False 
+		if (0 <= row < self.height) and (0 <= col < self.width):
+			return True
+		else:
+			return False			   
+	def was_already_guessed(self, row, col):
+		if self.grid[row][col] == "X":
+			return True
+		else:
+			return False
+	def to_string(self):
+		pass	
